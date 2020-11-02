f477076dfa2151b1d973522e1d2752b2e671bb55
fengcode <fengcode@hotmail.com>
2020-11-02 16:15:20 +0800

    提交

---
+++ FengCode.Libs.Image/FengCode.Libs.Image/ImageWrapper.cs
@@ -0,0 +1,139 @@
+﻿using FengCode.Libs.Image.Internal;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FengCode.Libs.Image
+{
+    /// <summary>
+    /// 图片文件包装器
+    /// </summary>
+    public class ImageWrapper
+    {
+        /// <summary>
+        /// 图片二进制内容
+        /// </summary>
+        public byte[] ImageBytes { get; protected set; }
+        /// <summary>
+        /// 使用图片内容创建对象实例
+        /// </summary>
+        /// <param name="imageBytes"></param>
+        public ImageWrapper(byte[] imageBytes)
+        {
+            if (imageBytes == null)
+            {
+                throw new ArgumentNullException(nameof(imageBytes));
+            }
+
+            ImageBytes = imageBytes;
+        }
+
+        /// <summary>
+        /// 通过图片文件创建对象实例
+        /// </summary>
+        public static ImageWrapper CreateFromFile(FileInfo imageFile)
+        {
+            if (imageFile == null)
+            {
+                throw new ArgumentNullException(nameof(imageFile));
+            }
+
+            if (!imageFile.Exists)
+            {
+                throw new FileNotFoundException("指定的图片文件不存在", imageFile.FullName);
+            }
+
+            byte[] bytes = File.ReadAllBytes(imageFile.FullName);
+            return new ImageWrapper(bytes);
+        }
+
+        /// <summary>
+        /// 通过验证码创建对象实例
+        /// </summary>
+        public static ImageWrapper CreateByVerifyCode(string code, int height = 38, int width = 120)
+        {
+            ImageVerifyCode verifyCode = new ImageVerifyCode(code, height, width);
+            byte[] bytes = verifyCode.BuildImage();
+            return new ImageWrapper(bytes);
+        }
+
+        /// <summary>
+        /// 创建内存流
+        /// </summary>
+        public MemoryStream CreateMemoryStream()
+        {
+            return new MemoryStream(this.ImageBytes.ToArray());
+        }
+
+        /// <summary>
+        /// 将图片保存为文件
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void SaveToFile(string filePath)
+        {
+            File.WriteAllBytes(filePath, this.ImageBytes);
+        }
+
+        /// <summary>
+        /// 调整图片的尺寸
+        /// </summary>
+        /// <param name="maxWidth">最大宽度</param>
+        /// <param name="maxHeight">最大高度</param>
+        /// <param name="isSameRate">是否保持比率</param>
+        /// <returns></returns>
+        public ImageWrapper Resize(int maxWidth, int maxHeight, bool isSameRate = false)
+        {
+            using (var stream = new MemoryStream(this.ImageBytes.ToArray()))
+            {
+                ImageSize imageSize = new ImageSize(maxWidth, maxHeight);
+                byte[] newBytes = ImageResize.Resize(stream, imageSize, isSameRate);
+                return new ImageWrapper(newBytes);
+            }
+        }
+
+        /// <summary>
+        /// 裁剪图片
+        /// </summary>
+        /// <param name="width">宽度</param>
+        /// <param name="height">高度</param>
+        /// <param name="quality">图片质量</param>
+        /// <returns></returns>
+        public ImageWrapper Crop(int width, int height, int quality = 100)
+        {
+            using (var stream = new MemoryStream(this.ImageBytes.ToArray()))
+            {
+                ImageSize imageSize = new ImageSize(width, height);
+                byte[] bytes = ImageResize.Crop(stream, imageSize, quality);
+                return new ImageWrapper(bytes);
+            }
+        }
+
+        /// <summary>
+        /// 是否是真实图片
+        /// </summary>
+        public bool IsReallyImage()
+        {
+            using (var memoryStream = new MemoryStream(this.ImageBytes.ToArray()))
+            {
+                using (var stream = new SKManagedStream(memoryStream)) //对流进行包装
+                {
+                    using (SKBitmap bitmap = SKBitmap.Decode(stream))
+                    {
+                        if (bitmap != null)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # FengCode.Libs.Image
-.Net 图片处理库
+
+## 介绍
+
+.net 图片处理类，基于netstandard2.0，通过一个类`ImageWrapper`，提供以下功能：
+
+- 创建验证码图片
+- 图片保存为文件
+- 调整图片尺寸大小
+- 裁剪图片
+- 判断一个文件是否是真实图片
+
+## 安装
+
+```
+PM> Install-Package FengCode.Libs.Image
+```
+
+
+
+## Linux部署
+
+linux环境下必须上传[libSkiaSharp.so](https://github.com/mono/SkiaSharp/releases/tag/v1.68.0)文件在项目目录内。
+
